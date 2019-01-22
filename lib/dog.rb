b0affd05119a884ded806855b796267359172ddb
Stephen Wright <scw248@gmail.com>
2019-01-22 15:04:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,3 +7,19 @@ class Person
     @name
   end
 end
+
+
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
+end
